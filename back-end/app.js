324f23822102d265fca2ebb768cd05a9a101b936
CotetoPraca <lucaspraca80@gmail.com>
2023-06-04 12:11:20 -0300

    back-end: update app.js with the new invetory-controller reference

--- back-end/app.js
+++ back-end/app.js
@@ -2,9 +2,6 @@ const express = require('express');
 const app = express();
 const mqttServer = require('./mqtt-server/mqtt-server');
 const inventoryController = require('./service-inventory/controllers/inventory-controller');
-const mqttHistoryService = require('./mqtt-server/mqtt-services/mqtt-history');
-const mqttHistoryCleanupService = require('./mqtt-server/mqtt-services/mqtt-history-cleanup');
-const helloWorldService = require('./service-inventory/hello-world');
 
 // Middleware para fazer o parsing do corpo das requisições como JSON
 app.use(express.json());
@@ -32,19 +29,20 @@ app.get('/services', (req, res) => {
 
 // Rota para exibir os resultados de processamento das mensagens MQTT
 app.get('/mqtt/history', (req, res) => {
-  mqttHistoryService.showHistory();
-  res.send('Histórico de resultados do MQTT limpo com sucesso');
+  logFilePath = '../logs/mqtt-server-logs/mqtt-results.log';
+  inventoryController.getServiceByName('log-get-results').handler(logFilePath);
 });
 
 // Rota para limpar o histórico de resultados
 app.get('/mqtt/history-cleanup', (req, res) => {
-  mqttHistoryCleanupService.historyCleanup();
+  logFilePath = '../logs/mqtt-server-logs/mqtt-results.log';
+  inventoryController.getServiceByName('log-clear-results').handler(logFilePath);
   res.send('Histórico de resultados do MQTT limpo com sucesso');
 });
 
 // Rota para chamar o serviço hello-world
 app.get('/hello-world', (req, res) => {
-  helloWorldService.helloWorld();
+  inventoryController.getServiceByName('hello-world');
 });
 
 // Inicia o servidor na porta 3000
