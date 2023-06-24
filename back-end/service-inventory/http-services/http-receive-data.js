5c9143b5b683631eee19c225e7725efbccaac930
CotetoPraca <lucaspraca80@gmail.com>
2023-06-24 18:30:42 -0300

    update http-receive-data with Promise

--- back-end/service-inventory/http-services/http-receive-data.js
+++ back-end/service-inventory/http-services/http-receive-data.js
@@ -11,27 +11,42 @@ const http = require('http');
 // Função principal do serviço de recebimento de dados
 // Recebe o endereço de origem para receber os dados por HTTP
 function receberDadosPorHTTP(origem) {
-  const server = http.createServer((req, res) => {
-    let body = '';
+  return new Promise((resolve, reject) => {
+    const server = http.createServer((req, res) => {
+      let body = '';
 
-    req.on('data', (chunk) => {
-      body += chunk;
+      req.on('data', (chunk) => {
+        body += chunk;
+      });
+
+      req.on('end', () => {
+        const dadosRecebidos = JSON.parse(body);
+
+        console.log('Dados recebidos:', dadosRecebidos);
+
+        // Retorna os dados recebidos como resposta
+        const resposta = {
+          dadosRecebidos: dadosRecebidos,
+          mensagem: 'Dados recebidos com sucesso',
+        };
+
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify(resposta));
+
+        // Resolve a Promise com os dados recebidos
+        resolve(dadosRecebidos);
+      });
     });
 
-    req.on('end', () => {
-      const dadosRecebidos = JSON.parse(body);
-      
-      console.log('Dados recebidos:', dadosRecebidos);
-
-      // Retorna os dados recebidos como resposta
-      res.statusCode = 200;
-      res.setHeader('Content-Type', 'application/json');
-      res.end(JSON.stringify(dadosRecebidos));
+    server.listen(origem.port, origem.hostname, () => {
+      console.log(`Servidor de recebimento iniciado em ${origem.hostname}:${origem.port}`);
     });
-  });
 
-  server.listen(origem.port, origem.hostname, () => {
-    console.log(`Servidor de recebimento iniciado em ${origem.hostname}:${origem.port}`);
+    // Lida com possíveis erros no servidor
+    server.on('error', (error) => {
+      reject(error);
+    });
   });
 }
 
@@ -47,3 +62,16 @@ inventoryController.addService({
 module.exports = {
   receberDadosPorHTTP,
 };
+
+/* Como chamar a função desse serviço:
+*
+* receberDadosPorHTTP(origem)
+*   .then((dadosRecebidos) => {
+*      // Faça algo com os dados recebidos
+*      console.log('Dados recebidos:', dadosRecebidos);
+*    })
+*    .catch((error) => {
+*      // Lida com possíveis erros
+*      console.error('Erro:', error);
+*    });
+*/
