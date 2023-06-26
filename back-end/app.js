7d3847282e91cdd85a2b1df5f0e3ff09c5829c3f
CotetoPraca <lucaspraca80@gmail.com>
2023-06-26 20:01:15 -0300

    remove the MQTT server from the application and update the services call on the endpoints

--- back-end/app.js
+++ back-end/app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const app = express();
-const mqttServer = require('./mqtt-server/mqtt-server');
 const inventoryController = require('./service-inventory/controllers/inventory-controller');
 
 // Middleware para fazer o parsing do corpo das requisições como JSON
@@ -15,22 +14,20 @@ const servicesToLoad = [
   require('./service-inventory/log-services/log-get-results'),
   require('./service-inventory/log-services/log-open-space'),
   require('./service-inventory/log-services/log-save-results'),
-  require('./service-inventory/template-services/hello-world')
+  require('./service-inventory/template-services/hello-world'),
+  require('../front-end/login.html')
 ];
 
-// Configuração do servidor MQTT
-mqttServer.start();
-
-// Rota para a página de login
+// Rota para a página de login.
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/front-end/login.html');
+  res.sendFile(__dirname + '../front-end/login.html');
 });
 
-//rota para a página inicial. É Feito a verificação se o usuário está logado
+// Rota para a página inicial. Verifica se o usuário está logado.
 app.get('/menu', (req, res) => {
   const token = req.query.token;
-  if (validarToken(token)) {
-    res.sendFile(__dirname + '/front-end/menu.html');
+  if (inventoryController.getServiceByName("auth-token-storage").verificarTokenValido(token)) {
+    res.sendFile(__dirname + '../front-end/menu.html');
   } else {
     res.status(401).send('Faça login antes de continuar.');
   }
@@ -46,68 +43,68 @@ app.post('/services', (req, res) => {
 // Rota para listar todos os serviços
 app.get('/services', (req, res) => {
   const token = req.query.token;
-  if (validarToken(token)) {
-  const services = inventoryController.getAllServices();
-  res.send(services); 
+  if (inventoryController.getServiceByName("auth-token-storage").verificarTokenValido(token)) {
+    const services = inventoryController.getAllServices();
+    res.send(services);
   } else {
-  res.status(401).send('Faça login antes de continuar.');
-}
+    res.status(401).send('Faça login antes de continuar.');
+  }
 });
 
 // Rota para exibir os resultados de processamento das mensagens MQTT. rota interna
-app.get('/mqtt/history', (req, res) => {
-  const token = req.query.token;
-  if (validarToken(token)) {
-  logFilePath = '../logs/mqtt-server-logs/mqtt-results.log';
-  const result = inventoryController.getServiceByName('log-get-results').handler(logFilePath);
-  res.send(result);
-} else {
-  res.status(401).send('Faça login antes de continuar.');
-}
-});
+// app.get('/mqtt/history', (req, res) => {
+//   const token = req.query.token;
+//   if (inventoryController.getServiceByName("auth-token-storage").verificarTokenValido(token)) {
+//     logFilePath = '../logs/mqtt-server-logs/mqtt-results.log';
+//     const result = inventoryController.getServiceByName('log-get-results').handler(logFilePath);
+//     res.send(result);
+//   } else {
+//     res.status(401).send('Faça login antes de continuar.');
+//   }
+// });
 
 //rota para exibir os resultados do processamento das mensagens MQTT. rota para o cliente.
-app.get('/mqtt/history-page', (req, res) => {
-  const token = req.query.token;
-  if (validarToken(token)) {
-    res.sendFile(__dirname + '/front-end/view.html');
-} else {
-  res.status(401).send('Faça login antes de continuar.');
-}
-});
+// app.get('/mqtt/history-page', (req, res) => {
+//   const token = req.query.token;
+//   if (inventoryController.getServiceByName("auth-token-storage").verificarTokenValido(token)) {
+//     res.sendFile(__dirname + '../front-end/view.html');
+//   } else {
+//     res.status(401).send('Faça login antes de continuar.');
+//   }
+// });
 
 // Rota para limpar o histórico de resultados
-app.get('/mqtt/history-cleanup', (req, res) => {
-  const token = req.query.token;
-  if (validarToken(token)) {
-  logFilePath = '../logs/mqtt-server-logs/mqtt-results.log';
-  const result = inventoryController.getServiceByName('log-clear-results').handler(logFilePath);
-  res.send(result);
-} else {
-  res.status(401).send('Faça login antes de continuar.');
-}
-});
+// app.get('/mqtt/history-cleanup', (req, res) => {
+//   const token = req.query.token;
+//   if (inventoryController.getServiceByName("auth-token-storage").verificarTokenValido(token)) {
+//     logFilePath = '../logs/mqtt-server-logs/mqtt-results.log';
+//     const result = inventoryController.getServiceByName('log-clear-results').handler(logFilePath);
+//     res.send(result);
+//   } else {
+//     res.status(401).send('Faça login antes de continuar.');
+//   }
+// });
 
 // Rota para acessar a página hello-world
 app.get('/hello-world-page', (req, res) => {
   const token = req.query.token;
-  if (validarToken(token)) {
-  res.sendFile(__dirname + '/front-end/helloworld.html');
+  if (inventoryController.getServiceByName("auth-token-storage").verificarTokenValido(token)) {
+    res.sendFile(__dirname + '../front-end/helloworld.html');
   } else {
-  res.status(401).send('Faça login antes de continuar.');
-}
+    res.status(401).send('Faça login antes de continuar.');
+  }
 });
 
-//rota interna para proceesar o hello world
+//rota interna para processar o serviço hello world
 app.get('/hello-world', (req, res) => {
   const token = req.query.token;
-  if (validarToken(token)) { 
-  const service = inventoryController.getServiceByName('hello-world');
-  const result = service.handler();
-  res.send(result);
-} else {
-  res.status(401).send('Faça login antes de continuar.');
-}
+  if (inventoryController.getServiceByName("auth-token-storage").verificarTokenValido(token)) {
+    const service = inventoryController.getServiceByName('hello-world');
+    const result = service.handler();
+    res.send(result);
+  } else {
+    res.status(401).send('Faça login antes de continuar.');
+  }
 });
 
 // Rota para autenticar o usuário
@@ -117,11 +114,6 @@ app.get('/auth/user', (req, res) => {
   const result = service.handler(username, password);
   res.send(result);
 });
-
-function validarToken(token) {
-  const service = inventoryController.getServiceByName('auth-user');
-  return service.validateToken(token);
-}
 
 // Inicia o servidor na porta 3000
 app.listen(3000, () => {
